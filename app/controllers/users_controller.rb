0ffba8b9585cc50ca56861030c9169991d5e23d9
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-05-31 11:53:50 +0200

    methods and template view#user#show#

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[edit update show]
+
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to user_path(@user)
     else
@@ -13,12 +13,21 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @buyer_orders = @user.orders.order(created_at: :desc)
+    @products = @user.products.order(created_at: :desc)
+    @seller_orders = @user.products.orders.order(created_at: :desc)
+    @reviews = @user.reviews.order(created_at: :desc)
+    @bookmarks = @user.bookmarks.order(created_at: :desc)
+    raise
   end
 
   private
 
-  def user_params
+  def set_user
+    @user = User.find(params[:id])
+  end
 
+  def user_params
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :address, :password, :password_confirmation)
   end
 end
