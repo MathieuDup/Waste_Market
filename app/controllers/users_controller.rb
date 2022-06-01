d0f4c981be2ea1a143fe860dafb5acc9a81bafaa
Florian Carriere <carriere.florian@gmail.com>
2022-06-01 17:11:13 +0200

    create order completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @buyer_orders = current_user.orders.where.not(progress: "Cancelled").order(created_at: :desc)
+    # @buyer_orders = current_user.orders.where.not(progress: "Cancelled").order(created_at: :desc)
+    @buyer_orders = current_user.orders.order(created_at: :desc)
     @products = current_user.products.order(created_at: :desc)
-    @seller_orders = current_user.orders_as_owner.where.not(progress: "Cancelled").order(created_at: :desc)
+    # @seller_orders = current_user.orders_as_owner.where.not(progress: "Cancelled").order(created_at: :desc)
+    @seller_orders = current_user.orders_as_owner.order(created_at: :desc)
     @reviews = current_user.my_reviews
     @bookmarks = current_user.bookmarks.order(created_at: :desc)
   end
