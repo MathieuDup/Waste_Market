b77a161837ca77e09395fe8dd6ec8e511da47e17
Florian Carriere <carriere.florian@gmail.com>
2022-06-01 11:16:05 +0200

    fix bug photo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :first_name, :last_name, :email, :address, :password, :password_confirmation)
+    params.require(:user).permit(:username, :first_name, :last_name, :email, :address, :password, :password_confirmation, :photo)
   end
 end
