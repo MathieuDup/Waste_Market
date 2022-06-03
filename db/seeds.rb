6ed9567a5d4dca134108889961b937a4aa0c3618
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-06-03 16:30:09 +0200

    correction seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,8 +22,8 @@ puts "Create users"
 admin = User.create(username: "admin", first_name: "Admin", last_name: "Admin", email: "admin@wastemarket.lol", password: "secret", address: "Place des Quinconces, Bordeaux")
 emy = User.create(username: "Emy", first_name: "Emy", last_name: "Emy", email: "emy@wastemarket.lol", password: "secret", address: "Cours Victor Hugo, Bordeaux")
 jim = User.create(username: "Jim", first_name: "Jim", last_name: "Jim", email: "jim@wastemarket.lol", password: "secret", address: "Quai Bacalan, Bordeaux")
-florian = User.create(username: "Florian", first_name: "Florian", last_name: "Florian", email: "florian@waste.market.lol", password: "secret", address: "Rue Gabriel Dedieu, Lormont")
-patricia = User.create(username: "Patricia", first_name: "Patricia", last_name: "Patricia", email: "patricia@waste.market.lol", password: "secret", address: "Esplanade des Antilles, Pessac")
+florian = User.create(username: "Florian", first_name: "Florian", last_name: "Florian", email: "florian@wastemarket.lol", password: "secret", address: "Rue Gabriel Dedieu, Lormont")
+patricia = User.create(username: "Patricia", first_name: "Patricia", last_name: "Patricia", email: "patricia@wastemarket.lol", password: "secret", address: "Esplanade des Antilles, Pessac")
 jérôme = User.create(username: "Jérôme", first_name: "Jérôme", last_name: "Jérôme ", email: "jerome@wastemarket.lol", password: "secret", address: "Rue Calixte-Camelle, Bègles")
 mathieu = User.create(username: "Mathieu", first_name: "Mathieu", last_name: "Mathieu", email: "mathieu@wastemarket.lol", password: "secret", address: "Rue Sainte Catherine, Bordeaux")
 
