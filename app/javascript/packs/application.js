ae641ca8e8c8d5b527d15241b8d83b26eb42be97
Jerome De Toffoli <j.detoffoli@orange.fr>
2022-06-06 14:25:11 +0200

    card product bis

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,3 +14,40 @@ ActiveStorage.start()
 
 import "controllers"
 import "bootstrap"
+
+import { initSweetalert } from '../plugins/init_sweetalert';
+
+//Button validate
+initSweetalert('#sweet-alert-demo', {
+  title: "A nice alert",
+  text: "This is a great alert, isn't it?",
+  icon: "success"
+});
+
+//Button reject
+initSweetalert('#sweet-alert-reject', {
+  title: "A nice reject",
+  text: "This is a great alert reject, isn't it?",
+  icon: "success"
+});
+
+//Button cancel
+initSweetalert('#sweet-alert-cancel', {
+  title: "A nice cancel",
+  text: "This is a alert cancel, isn't it?",
+  icon: "success"
+});
+
+//Button delete
+initSweetalert('#sweet-alert-delete', {
+  title: "Are you sure?",
+  text: "This action cannot be reversed",
+  icon: "warning"
+});
+
+//Button order
+initSweetalert('#sweet-alert-order', {
+  title: "Are you sure?",
+  text: "This is a alert order",
+  icon: "success"
+});
