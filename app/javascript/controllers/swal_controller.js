4b906e5acc5dfbf78059f81868d210df486fa08a
Jerome De Toffoli <j.detoffoli@orange.fr>
2022-06-07 17:08:00 +0200

    sweetalert2 ok + sweetalert1 removed

--- app/javascript/controllers/swal_controller.js
+++ app/javascript/controllers/swal_controller.js
@@ -1,9 +1,23 @@
 import { Controller } from "stimulus"
+import Swal from 'sweetalert2'
 
 export default class extends Controller {
-  static targets = [ "" ]
 
-  connect() {
-    console.log('coucou')
+  static targets = ["swalform"]
+
+  alert(event) {
+    event.preventDefault();
+    Swal.fire({
+      icon: 'info',
+      html: `<p>You're about to create a new product, are you sure ?</p>`,
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Yes!'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire('Congratulation', "You've just create your product !", 'success').then(() => { this.swalformTarget.submit() });
+      }
+    })
   }
 }
