413ece842cf01648d95e45a097b9550f5842ddd0
Mathieu Dupin <dupin.mathieu@gmail.com>
2022-06-07 15:25:47 +0200

    done

--- app/javascript/controllers/quantity_left_controller.js
+++ app/javascript/controllers/quantity_left_controller.js
@@ -4,14 +4,14 @@ export default class extends Controller {
   static targets = [ "quantity", "price" ]
 
   test() {
-    console.log(this.quantityTarget.value)
-    //  récupérer price du product data-value stimulus
+  //  récupérer price du product data-value stimulus
     this.price = Number(this.element.dataset.price)
     console.log(this.price)
   //  calculer la décote du prix
     this.discountPrice = this.price * (this.quantityTarget.value / 100)
   //  insérer dans l'html de l'input price
-    this.priceTarget.value = this.discountPrice
+    if (this.price > 0) {
+      this.priceTarget.value = this.discountPrice
+    }
   }
-
 }
