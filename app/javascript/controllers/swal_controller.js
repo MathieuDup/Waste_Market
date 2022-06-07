ea71d591d966df51ddb879636098e64fa1dcf9d5
Jerome De Toffoli <j.detoffoli@orange.fr>
2022-06-07 18:33:10 +0200

    sweet alert congratulation

--- app/javascript/controllers/swal_controller.js
+++ app/javascript/controllers/swal_controller.js
@@ -8,12 +8,18 @@ export default class extends Controller {
   alert(event) {
     event.preventDefault();
     Swal.fire({
-      icon: 'info',
-      html: `<p>You're about to create a new product, are you sure ?</p>`,
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes!'
+      position: 'center',
+      icon: 'success',
+      title: 'Your product has been created',
+      showConfirmButton: true
+
+      //title: 'Are you sure?',
+      //icon: 'success',
+     // html: `<p>You're about to create a new product, are you sure ?</p>`,
+      // showCancelButton: true,
+      // confirmButtonColor: '#3085d6',
+      // cancelButtonColor: '#d33',
+      //confirmButtonText: 'Yes, create it!'
     }).then((result) => {
       if (result.isConfirmed) {
         Swal.fire('Congratulation', "You've just create your product !", 'success').then(() => { this.swalformTarget.submit() });
